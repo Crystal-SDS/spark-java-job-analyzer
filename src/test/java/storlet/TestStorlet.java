5a67d0c50c7b1ba36a2dcecede95e4b0968adebc
raulgracia <raulgraciatinedo@gmail.com>
2017-06-02 11:51:18 +0200

    Add test to storlet to profile and improve performance.

---
+++ src/test/java/storlet/TestStorlet.java
@@ -0,0 +1,93 @@
+package test.java.storlet;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.ibm.storlet.common.StorletInputStream;
+import com.ibm.storlet.common.StorletLogger;
+import com.ibm.storlet.common.StorletObjectOutputStream;
+import com.ibm.storlet.common.StorletOutputStream;
+
+public class TestStorlet {
+	
+	public static final String INPUT_FILE_NAME = "test_data/meter_gen.csv";
+	public static final String OUTPUT_FILE_NAME = "test_data/meter.results";
+	public static final String OUTPUT_MD_FILE_NAME = "test_data/output_record_md.txt";
+	public static final String LOGGER_FILE_NAME = "test_data/logger";	
+	
+	public static void main(String[] args) {
+		System.out.println("entering main");
+		try {
+
+			FileInputStream infile = new FileInputStream(INPUT_FILE_NAME);
+			FileOutputStream outfile = new FileOutputStream(OUTPUT_FILE_NAME);
+			FileOutputStream outfile_md = new FileOutputStream(OUTPUT_MD_FILE_NAME);
+
+			HashMap<String, String> md = new HashMap<String, String>();
+			StorletInputStream inputStream1 = new StorletInputStream(infile.getFD(), md);
+	        StorletObjectOutputStream outStream = new StorletObjectOutputStream(outfile.getFD(), md, outfile_md.getFD());
+	        
+	        ArrayList<StorletInputStream> inputStreams = new ArrayList<StorletInputStream>();
+	        inputStreams.add(inputStream1);	        
+	        ArrayList<StorletOutputStream> outStreams = new ArrayList<StorletOutputStream>();
+	        outStreams.add(outStream);
+	        
+	        //LambdaPushdownStorlet storlet; //new NoopStorlet();
+	        LambdaPushdownStorlet storlet = new LambdaPushdownStorlet();
+	        
+			FileOutputStream loggerFile = new FileOutputStream(LOGGER_FILE_NAME);					
+			StorletLogger logger = new StorletLogger(loggerFile.getFD());				
+			Map<String, String> parameters = new HashMap<String, String>();	
+			
+			parameters.put("1-lambda", "java.util.function.Predicate<java.lang.String>|filter(s -> s.contains(\"2015\"))");
+			//parameters.put("2-map", "s -> s + \"1234563564545\"");
+			//parameters.put("3-filter", "s -> s.contains(\"B\")");	
+			//parameters.put("4-map", "s -> s + \"aaaaaaaa\"");
+			//parameters.put("5-filter", "s -> s.contains(\"A\")");	
+			
+			System.out.println("before storlet");
+			long iniTime = System.nanoTime();
+			storlet.invoke(inputStreams, outStreams, parameters, logger);
+			System.out.println("after storlet: " + (System.nanoTime()-iniTime)/1000000.);			
+			
+			infile.close();
+			outfile.close();
+			outfile_md.close();
+			
+			for (int i=0; i<5; i++){
+			
+				infile = new FileInputStream(INPUT_FILE_NAME);
+				outfile = new FileOutputStream(OUTPUT_FILE_NAME);
+				outfile_md = new FileOutputStream(OUTPUT_MD_FILE_NAME);
+				inputStream1 = new StorletInputStream(infile.getFD(), md);
+		        outStream = new StorletObjectOutputStream(outfile.getFD(), md, outfile_md.getFD());	        
+		        inputStreams = new ArrayList<StorletInputStream>();
+		        inputStreams.add(inputStream1);	        
+		        outStreams = new ArrayList<StorletOutputStream>();
+		        outStreams.add(outStream);
+		        
+		        System.out.println("before storlet");
+		        iniTime = System.nanoTime();
+				storlet.invoke(inputStreams, outStreams, parameters, logger);
+				System.out.println("after storlet: " + (System.nanoTime()-iniTime)/1000000.);
+		        
+		        infile.close();
+				outfile.close();
+				outfile_md.close();
+			}
+			
+			loggerFile.close();
+			
+		}
+		catch (Exception e) {
+			System.out.println("had an exception");
+			e.printStackTrace();
+		}
+		System.out.println("exiting main");
+	}
+
+
+}
