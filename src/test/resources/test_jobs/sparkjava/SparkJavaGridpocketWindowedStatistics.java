16819d96213a2dcfec776d001bc0ab778e5fc3c7
raulgracia <raulgraciatinedo@gmail.com>
2017-10-03 16:32:53 +0200

    New version of GridPocket Job.

--- src/test/resources/test_jobs/sparkjava/SparkJavaGridpocketWindowedStatistics.java
+++ src/test/resources/test_jobs/sparkjava/SparkJavaGridpocketWindowedStatistics.java
@@ -17,9 +17,9 @@ public class SparkJavaGridpocketWindowedStatistics {
 		
 		SparkConf conf = new SparkConf().setAppName("SimpleTextAnalysisSparkJava");
 		JavaSparkContext sc = new JavaSparkContext(conf);
-		JavaRDD<String> distFile = sc.textFile("swift2d://gridpocket_140GB.lvm/*");
-		List<Tuple2<String, Double>> sortedMeterSlotMaxValues = distFile.
-				mapToPair(s -> {
+		JavaRDD<String> distFile = sc.textFile("swift2d://gridpocket_140GB.lvm/1.csv");
+		distFile.filter(s -> !s.startsWith("date"))
+				.mapToPair(s -> {
 					String[] split = s.split(",");
 					String meterSlotKey = null;
 					try {
@@ -29,42 +29,46 @@ public class SparkJavaGridpocketWindowedStatistics {
 					return new Tuple2<String, Double>(meterSlotKey, new Double(split[1]));
 				})
 				.reduceByKey((t1, t2) -> Math.max(t1, t2))
+				.mapToPair(t -> new Tuple2<String, Tuple2<String, Double>>(t._1.substring(0, t._1.indexOf("-")),
+							new Tuple2<String, Double>(t._1.substring(t._1.indexOf("-")+1), t._2)))
+				.groupByKey()
+				.mapPartitionsToPair(meterPartitions -> {
+					List<Tuple2<String, Iterable<Tuple2<String, Double>>>> finalResults = new ArrayList<>();
+					while (meterPartitions.hasNext()) {
+						Tuple2<String, Iterable<Tuple2<String, Double>>> meterTuple = meterPartitions.next();
+						List<Tuple2<String, Double>> perSlotMeterEnergy = new ArrayList<>();
+						Double previousMeter = null;
+						for (Tuple2<String, Double> slotTuple: meterTuple._2()) {	
+							if (previousMeter!=null){	
+								perSlotMeterEnergy.add(new Tuple2<String, Double>(slotTuple._1(), slotTuple._2()-previousMeter));		
+							} else perSlotMeterEnergy.add(slotTuple);
+							previousMeter =  slotTuple._2();	
+						}
+						finalResults.add(new Tuple2<String, Iterable<Tuple2<String,Double>>>(meterTuple._1, perSlotMeterEnergy));
+					}
+					return finalResults.iterator();
+					}, true)
+				.flatMapToPair(meterPartitions -> meterPartitions._2.iterator())
+				.mapToPair(t -> {
+					Tuple4<Double, Double, Double, Long> values = new Tuple4<>(t._2, t._2, t._2, 1L);								
+					return new Tuple2<String, Tuple4>(t._1, values); 
+				})
+				.reduceByKey((t1, t2) -> {
+					Double sumEnergyPerSlot = ((Double)t1._1()) + ((Double)t2._1());
+					Double minEnergyPerSlot = Math.min((Double)t1._2(), (Double)t2._2());
+					Double maxEnergyPerSlot = Math.max((Double)t1._3(), (Double)t2._3());
+					Long count = ((Long)t1._4()) + ((Long)t2._4());
+					return new Tuple4<Double, Double, Double, Long>(
+							sumEnergyPerSlot, minEnergyPerSlot, maxEnergyPerSlot, count);
+				})
 				.sortByKey()
-				.collect();
-				
-		String previousMeter = null, currentMeter = null;
-		List<Tuple2<String, Double>> sortedMeterSlotCleanValues = new ArrayList<>();
-		for(int i=0; i<sortedMeterSlotMaxValues.size()-1; i++) {
-			currentMeter = sortedMeterSlotMaxValues.get(i)._1().substring(
-					0, sortedMeterSlotMaxValues.get(i)._1().indexOf("-"));
-			if (previousMeter!=null && previousMeter.equals(currentMeter)){	
-				sortedMeterSlotCleanValues.add(new Tuple2<String, Double>(sortedMeterSlotMaxValues.get(i)._1(), 
-					sortedMeterSlotMaxValues.get(i)._2()-sortedMeterSlotMaxValues.get(i-1)._2()));		
-			} else sortedMeterSlotCleanValues.add(sortedMeterSlotMaxValues.get(i));
-			previousMeter = currentMeter;	
-		}
-		sortedMeterSlotMaxValues.clear();
-		
-		sc.parallelizePairs(sortedMeterSlotCleanValues)
-					.mapToPair(t -> {
-						Tuple4<Double, Double, Double, Long> values = new Tuple4<>(t._2, t._2, t._2, 1L);								
-						return new Tuple2<String, Tuple4>(t._1.substring(t._1.indexOf("=")+1), values); 
-					})
-					.reduceByKey((t1, t2) -> {
-						Double sumEnergyPerSlot = ((Double)t1._1()) + ((Double)t2._1());
-						Double minEnergyPerSlot = Math.min((Double)t1._2(), (Double)t2._2());
-						Double maxEnergyPerSlot = Math.max((Double)t1._3(), (Double)t2._3());
-						Long count = ((Long)t1._4()) + ((Long)t2._4());
-						return new Tuple4<Double, Double, Double, Long>(
-								sumEnergyPerSlot, minEnergyPerSlot, maxEnergyPerSlot, count);
-					})
-					.sortByKey()
-					.map(t -> {
-						Tuple4<Double, Double, Double, Long> values = t._2;
-						return new Tuple2<String, Tuple3<Double, Double, Double>>(t._1,
-							new Tuple3<>((Double)values._1()/values._4(), (Double)values._2(), (Double)values._3()));
-					})
-					.saveAsTextFile("swift2d://output_pushdown.lvm/gridpocket_timeslot_results.csv");		
+				.map(t -> {
+					Tuple4<Double, Double, Double, Long> values = t._2;
+					String date = Instant.ofEpochMilli(Long.valueOf(t._1)*24*3600*1000).toString();
+					return new Tuple2<String, Tuple3<Double, Double, Double>>(date,
+						new Tuple3<>((Double)values._1()/values._4(), (Double)values._2(), (Double)values._3()));
+				})
+				.saveAsTextFile("swift2d://output_pushdown.lvm/gridpocket_timeslot_results.csv");
 	}
 
 }
